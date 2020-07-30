70046d807ed3f327db9c1542d4d9357c39f7f2eb
cendrars59 <cyr.leroux.dev@moinslequart.net>
2020-07-30 18:30:49 +0200

    fixing static

--- config/settings.py
+++ config/settings.py
@@ -142,7 +142,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 SATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/dist'),)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
